b55b1ed1bee576920cea18c83bc37d2a0f52c09f
Mauro Insaurralde <insa.mauro.94@gmail.com>
2026-03-11 16:57:03 -0300

    cambios

--- app/alumno/[curso]/page.tsx
+++ app/alumno/[curso]/page.tsx
@@ -1,10 +1,15 @@
 import { StudentClassView } from "@/components/student-class-view"
 
 interface PageProps {
-  params: Promise<{ curso: string }>
+  params: Promise<{
+    curso: string
+  }>
 }
 
 export default async function AlumnoClasePage({ params }: PageProps) {
+
   const { curso } = await params
+
   return <StudentClassView curso={curso} />
+
 }
